c8773f56c12bde2385299e780ca9cd7159cb0339
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2022-10-09 23:11:57 -0500

    Add files via upload

---
+++ Chat-Bot__Discord-Connection__/Project/greetings.py
@@ -0,0 +1,26 @@
+x_json = """{"data":[
+    {
+    "response_type": "greeting",
+    "user_input": ["hello", "hi", "hey"],
+    "bot_response": "Hey there!",
+    "required_words": []
+  },
+  {
+    "response_type": "greeting",
+    "user_input": ["see you", "goodbye", "bye", "quit", "q"],
+    "bot_response": "See you later!",
+    "required_words": []
+  },
+  {
+    "response_type": "greeting",
+    "user_input": ["nice", "to", "meet", "you"],
+    "bot_response": "The pleasure is all mine!",
+    "required_words": ["nice", "meet", "you"]
+  },
+  {
+    "response_type": "greeting",
+    "user_input": ["nice", "good", "i love you", "excellent", "wow", "amazing", "beautiful",
+                   "pretty", "handsome", "beautifull", "thanks", "bravo"],
+    "bot_response": "You are welcome! :) ",
+    "required_words": []
+  }"""
